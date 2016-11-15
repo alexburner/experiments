490347176a37083832b80c393d33658d0dcc631a
Alex Burner <alex.burner@gmail.com>
2016-11-15 01:15:22 -0800

    adding paperjs/genesis

---
+++ paperjs/genesis/in-place/js/RectGrid.js
@@ -0,0 +1,43 @@
+function RectGrid(origin, size, padding, rowCount, colCount) {
+	// properties
+	this.origin = origin;
+	this.size = size;
+	this.group = new paper.Group();
+	this.matrix = undefined;
+	// construction
+	this.makePanels(padding, rowCount, colCount);
+}
+
+RectGrid.prototype.makePanels = function(padding, rowCount, colCount) {
+
+	this.matrix = [];
+
+	var cellWidth = (this.size.width - padding) / colCount;
+	var cellHeight = (this.size.height - padding) / rowCount;
+
+	var currentX = this.origin.x;
+	var currentY = this.origin.y;
+
+	for (var i = 0; i < rowCount; i++) {
+		var rectRow = [];
+		for (var j = 0; j < colCount; j++) {
+
+			var rect = new paper.Rectangle(
+				new paper.Point(
+					currentX + padding,
+					currentY + padding
+				),
+				new paper.Size(
+					cellWidth - padding,
+					cellHeight - padding
+				)
+			);
+
+			rectRow.push(rect);
+			currentX += cellWidth;
+		}
+		this.matrix.push(rectRow);
+		currentY += cellHeight;
+		currentX = this.origin.x;
+	}
+};
