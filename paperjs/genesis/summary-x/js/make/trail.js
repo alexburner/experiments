ced26cb4745261b733d995217594dac234060b95
Alex Burner <alex.burner@gmail.com>
2018-11-16 21:59:31 -0800

    Allow unconfigured prettier

--- paperjs/genesis/summary-x/js/make/trail.js
+++ paperjs/genesis/summary-x/js/make/trail.js
@@ -2,16 +2,16 @@
 //
 /////////////////////////////////////////
 function makeTrail(args) {
-  makeBack(args)
+  makeBack(args);
 
-  var radius = args.radius || 16
-  var x = args.x
-  var y = args.y
-  var w = args.width
-  var h = args.height
-  var orient = args.orient || 'new'
-  var paddingV = args.paddingV || 120
-  var paddingH = args.paddingH || 120
+  var radius = args.radius || 16;
+  var x = args.x;
+  var y = args.y;
+  var w = args.width;
+  var h = args.height;
+  var orient = args.orient || "new";
+  var paddingV = args.paddingV || 120;
+  var paddingH = args.paddingH || 120;
 
   var grid = new RectGrid(
     new paper.Point(x, y),
@@ -19,14 +19,14 @@ function makeTrail(args) {
     paddingV,
     paddingH,
     7,
-    5,
-  )
+    5
+  );
 
   for (var i = 0; i < grid.matrix.length; i++) {
-    var stage = i + 1
+    var stage = i + 1;
     for (var j = 0; j < grid.matrix[i].length; j++) {
-      var rect = grid.matrix[i][j]
-      var genesis = undefined
+      var rect = grid.matrix[i][j];
+      var genesis = undefined;
       switch (j) {
         case 2:
           genesis = new GenesisPanel(
@@ -34,19 +34,19 @@ function makeTrail(args) {
             radius,
             rect.point,
             rect.size,
-            orient,
-          )
-          break
+            orient
+          );
+          break;
         default:
-          break
+          break;
       }
       if (genesis) {
         GenesisPanel.rotate(
           genesis.group,
           genesis.center,
           genesis.stage,
-          genesis.orient,
-        )
+          genesis.orient
+        );
       }
     }
   }
